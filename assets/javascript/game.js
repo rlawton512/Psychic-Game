db8e8a2ea80dcf29f6da44776daccf27759d8735
rlawton512 <rocsi54321@gmail.com>
2017-06-16 11:04:21 -0400

    wip v.3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,59 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 var wins = 0;
 var losses = 0;
-var left = 10;
-var guessed = [];
+var guessesLeft = 10;
+var currentGuesses = [];
+var computerGuess;
 
+function chooseComputerLetter() {
+	computerGuess  = alphabet[Math.floor(Math.random()* alphabet.length)];
+}
 
+function reset() {
+	guessesLeft=10;
+	currentGuesses= [];
+	chooseComputerLetter();
+}
+
+chooseComputerLetter();
 //have computer generate random letter of the alphabet 
 
 document.onkeyup = function(event) {
 
 	var userGuess = event.key;
 
-	var computerGuess = alphabet[Math.floor(Math.random()* alphabet.length)];
+	// if user pressed a valid key and userGuess does not exist in the guessed array
+	if (alphabet.indexOf(userGuess) !== -1 && currentGuesses.indexOf(userGuess) === -1) {
 
-	
-if (userGuess === "a","b","c","e","f","g","h","i","j","k","l","m","n","o","p",
-	"q","r","s","t","u","v","w","x","y","z") {
+		if (userGuess===computerGuess) {
+			wins++;
+			alert("Great job! You win!") 
+			reset();	
+		} else {
+			guessesLeft--;
+			currentGuesses.push(userGuess);
+		} 
 
-	if(userGuess===computerGuess){
-			wins++; 	
+		//for (var i=0; i<1; i++) {
 		
-	} else {
-		left--;
-
-for(var i=0; i<1; i++) {
-	guessed.push(userGuess);
+		//}		
 	}		
-}		
+
+	if (guessesLeft === 0) {
+		losses++;
+		reset();
+		alert("Game Over! Try again?");
+	}
+
+	var html = "<p>Guess what letter I'm thinking of...10 tries to win the game: ("+ computerGuess +")</p>" +
+	"<p>Wins: " + wins + "</p>" +
+	"<p>Losses: " + losses + "</p>" +
+	"<p>Guesses Left: " + guessesLeft + "</p>" +
+	"<p>Your Guesses so far: " + currentGuesses + "</p>";
+
+
+	document.getElementById("game").innerHTML = html;
 }
-
-
-var html = "<p>Guess what letter I'm thinking of:</p>" +
-"<p>Wins: " + wins + "</p>" +
-"<p>Losses: " + losses + "</p>" +
-"<p>Guesses Left: " + left + "</p>" +
-"<p>Your Guesses so far: " + guessed + "</p>";
-
-
-document.getElementById("game").innerHTML = html;}
 
 
 
